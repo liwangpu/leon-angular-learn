9a2f655eeb698dad688b4e9edba1e7cfecbb3a8d
leon <puliwang@cxist.com>
2020-05-19 23:40:45 +0800

    5

--- src/app/custom-route-reuse-strategy.service.ts
+++ src/app/custom-route-reuse-strategy.service.ts
@@ -6,37 +6,38 @@ import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from 
 })
 export class CustomRouteReuseStrategyService implements RouteReuseStrategy {
 
-    routesToCache: string[] = ["search", "list"];
-    storedRouteHandles = new Map<string, DetachedRouteHandle>();
-    constructor() { }
+    // routesToCache: string[] = ["search", "list"];
+    routesToCache: string[] = [];
+        storedRouteHandles = new Map<string, DetachedRouteHandle>();
+        constructor() {}
 
     // Decides if the route should be stored
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
-        return this.routesToCache.indexOf(route.routeConfig.path) > -1;
-    }
+            return this.routesToCache.indexOf(route.routeConfig.path) > -1;
+        }
 
     //Store the information for the route we're destructing
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-        console.log('shouldDetach', route);
-        this.storedRouteHandles.set(route.routeConfig.path, handle);
-    }
+            console.log('shouldDetach', route);
+            this.storedRouteHandles.set(route.routeConfig.path, handle);
+        }
 
     //Return true if we have a stored route object for the next route
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
-        console.log('shouldAttach', route);
-        return this.storedRouteHandles.has(route.routeConfig.path);
-    }
+            console.log('shouldAttach', route);
+            return this.storedRouteHandles.has(route.routeConfig.path);
+        }
 
     //If we returned true in shouldAttach(), now return the actual route data for restoration
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-        return this.storedRouteHandles.get(route.routeConfig.path);
-    }
+            return this.storedRouteHandles.get(route.routeConfig.path);
+        }
 
     //Reuse the route if we're going to and from the same route
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-        // console.log(1,this.storedRouteHandles.size);
-        return future.routeConfig === curr.routeConfig;
-    }
+            // console.log(1,this.storedRouteHandles.size);
+            return future.routeConfig === curr.routeConfig;
+        }
 
     // shouldDetach(route: ActivatedRouteSnapshot): boolean {
     //     // console.log('shouldDetach', route);
