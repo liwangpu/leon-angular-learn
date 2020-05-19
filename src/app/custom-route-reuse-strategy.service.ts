7b97798ef8ace3630d168ea0b32dfc1a534ea7b7
leon <puliwang@cxist.com>
2020-05-19 21:50:55 +0800

    5

--- src/app/custom-route-reuse-strategy.service.ts
+++ src/app/custom-route-reuse-strategy.service.ts
@@ -9,22 +9,27 @@ export class CustomRouteReuseStrategyService implements RouteReuseStrategy {
     constructor() { }
 
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
+        console.log('shouldDetach', route);
         return false;
     }
 
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-
+        console.log('store', route, handle);
     }
 
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
+        console.log('shouldAttach', route);
         return false;
     }
 
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+        console.log('retrieve', route);
         return null;
     }
 
-    shouldReuseRoute(before: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+        console.log('shouldReuseRoute future', future);
+        console.log('shouldReuseRoute curr', curr);
         return false;
     }
 }
--- src/app/customer/product/product.module.ts
+++ src/app/customer/product/product.module.ts
@@ -6,13 +6,21 @@ import { DynamicComponent } from './dynamic/dynamic.component';
 import { SearchComponent } from './search/search.component';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { RouteReuseStrategy } from '@angular/router';
+import { CustomRouteReuseStrategyService } from 'src/app/custom-route-reuse-strategy.service';
 
 
 @NgModule({
-  declarations: [DynamicComponent, SearchComponent, ListComponent, DetailComponent],
-  imports: [
-    CommonModule,
-    ProductRoutingModule
-  ]
+    declarations: [DynamicComponent, SearchComponent, ListComponent, DetailComponent],
+    imports: [
+        CommonModule,
+        ProductRoutingModule
+    ],
+    providers: [
+        // {
+        //     provide: RouteReuseStrategy,
+        //     useClass: CustomRouteReuseStrategyService
+        // }
+    ]
 })
 export class ProductModule { }
