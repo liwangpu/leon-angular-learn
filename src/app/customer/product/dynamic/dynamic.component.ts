14514357fbe9433e27f31c2979423f65fb219c6d
Leon Pu <puliwang@cxist.com>
2020-05-19 17:18:36 +0800

    2

--- src/app/customer/product/dynamic/dynamic.component.ts
+++ src/app/customer/product/dynamic/dynamic.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-dynamic',
-  templateUrl: './dynamic.component.html',
-  styleUrls: ['./dynamic.component.scss']
+    selector: 'app-dynamic',
+    templateUrl: './dynamic.component.html',
+    styleUrls: ['./dynamic.component.scss']
 })
-export class DynamicComponent implements OnInit {
+export class DynamicComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    key: string;
+    constructor(
+        private acr: ActivatedRoute
+    ) {
+        this.acr.paramMap.subscribe(res => {
+            // console.log(1, res);
+            this.key = res.get('key');
+        });
+    }
+    ngOnDestroy(): void {
+        console.log('dynamic destroy');
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        console.log('dynamic init');
+    }
 
 }
